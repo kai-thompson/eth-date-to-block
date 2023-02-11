07cc434513f3dea7221d05d64fb29b4b298e48d0
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-02-10 20:07:34 -0500

    adding position of block + semi-optimized batch blocks

--- src/lib/dater/index.ts
+++ src/lib/dater/index.ts
@@ -1,9 +1,9 @@
 import NodeClient from "../node-client";
 
-import { IEthereumDater, DaterBlockInfo, EthereumDaterConfig } from "./types";
+import { DaterBlockInfo, EthereumDaterConfig, IEthereumDater } from "./types";
 
-import * as constants from "./constants";
 import { Provider } from "../types";
+import * as constants from "./constants";
 
 class EthereumDater implements IEthereumDater {
     private nodeClient: NodeClient;
@@ -27,7 +27,10 @@ class EthereumDater implements IEthereumDater {
         this.maxRetries = maxRetries || constants.DEFAULT_MAX_RETRIES;
     }
 
-    async getBlock(date: number | string | Date): Promise<DaterBlockInfo> {
+    async getBlock(
+        date: number | string | Date,
+        position: string = "closest",
+    ): Promise<DaterBlockInfo> {
         await this.getInitialTimestamp();
         this.getTimestamp(date);
 
@@ -39,16 +42,82 @@ class EthereumDater implements IEthereumDater {
             currentBlock,
             0,
             totalTime / currentBlock,
+            position,
         );
 
         return resolvedBlock;
     }
 
+    async getBlocks(
+        startDate: number | string | Date,
+        endDate: number | string | Date,
+        secondsPerInterval: number,
+        position: string = "closest",
+    ): Promise<DaterBlockInfo[]> {
+        // make sure the interval is positive
+        if (secondsPerInterval <= 0) {
+            throw new Error("Interval must be greater than 0");
+        }
+        // make sure that endDate > startDate
+        const startTime = new Date(startDate).getTime();
+        const endTime = new Date(endDate).getTime();
+        if (endTime - startTime < 0) {
+            throw new Error(`${endDate} is not before ${startDate}`);
+        }
+        // get the bounds to reduce requests finding each block in the interval
+        // get the start block
+        const startBlock = await this.getBlock(startDate, position);
+        // get the end block
+        const endBlock = await this.getBlock(endDate, position);
+        // number of blocks we need to return
+        const n = Math.floor(
+            (endBlock.block.timestamp - startBlock.block.timestamp) /
+                secondsPerInterval,
+        );
+        // create return array
+        const blocks = new Array(n);
+        // get the block time
+        const timeDiff =
+            (endBlock.block.timestamp - this.initialTimestamp) /
+            endBlock.block.number;
+        // loop vars
+        let tempLo = startBlock.block.number;
+        let tempBlock = startBlock;
+        for (let i = 1; i < n - 1; i += 1) {
+            // required param
+            this.getTimestamp(
+                (startBlock.block.timestamp + i * secondsPerInterval) * 1000,
+            );
+            // Find Block: disable eslint because we try to optimize request count and not time taken
+            // eslint-disable-next-line no-await-in-loop
+            tempBlock = await this.checkBlock(
+                endBlock.block.number,
+                tempLo,
+                timeDiff,
+                position,
+            );
+            // set block
+            blocks[i] = tempBlock;
+            // set new lo
+            tempLo = tempBlock.block.number + 1;
+        }
+        // set start and end
+        blocks[0] = startBlock;
+        // only if there is more than one block
+        if (n > 1) {
+            blocks[n - 1] = endBlock;
+        }
+
+        return blocks;
+    }
+
+    // position can be "before", "after", "closest". Default "closest"
     private async checkBlock(
         high: number,
         low: number,
         blockTime: number,
-        depth = 0,
+        position: string = "closest",
+        depth: number = 0,
     ): Promise<DaterBlockInfo> {
         if (depth > this.maxRetries) {
             throw new Error(
@@ -56,12 +125,12 @@ class EthereumDater implements IEthereumDater {
             );
         }
 
-        const pos = Math.floor(
+        let pos = Math.floor(
             (this.timestamp - this.initialTimestamp) / blockTime,
         );
 
         const blockInfo = await this.nodeClient.getBlock(pos);
-        const currentBlockTimestamp = Number(blockInfo.timestamp);
+        let currentBlockTimestamp = Number(blockInfo.timestamp);
 
         const adjustedBlockTime =
             (currentBlockTimestamp - this.initialTimestamp) / pos;
@@ -71,30 +140,86 @@ class EthereumDater implements IEthereumDater {
             pos === low ||
             pos === high
         ) {
-            return {
-                block: {
-                    number: pos,
-                    timestamp: currentBlockTimestamp,
-                    date: new Date(currentBlockTimestamp * 1000),
-                },
-                retries: depth,
-                secondsFromTarget: Math.ceil(
-                    this.timestamp - currentBlockTimestamp,
-                ),
-            };
+            switch (position) {
+                case "before":
+                    if (currentBlockTimestamp > this.timestamp) {
+                        const prevBlock = await this.nodeClient.getBlock(
+                            pos - 1,
+                        );
+                        currentBlockTimestamp = Number(prevBlock.timestamp);
+                        depth += 1;
+                        pos -= 1;
+                    }
+                    return {
+                        block: {
+                            number: pos,
+                            timestamp: currentBlockTimestamp,
+                            date: new Date(currentBlockTimestamp * 1000),
+                        },
+                        retries: depth,
+                        secondsFromTarget: Math.ceil(
+                            this.timestamp - currentBlockTimestamp,
+                        ),
+                    };
+                case "after":
+                    if (currentBlockTimestamp < this.timestamp) {
+                        const nextBlock = await this.nodeClient.getBlock(
+                            pos + 1,
+                        );
+                        currentBlockTimestamp = Number(nextBlock.timestamp);
+                        depth += 1;
+                        pos += 1;
+                    }
+                    return {
+                        block: {
+                            number: pos,
+                            timestamp: currentBlockTimestamp,
+                            date: new Date(currentBlockTimestamp * 1000),
+                        },
+                        retries: depth,
+                        secondsFromTarget: Math.ceil(
+                            this.timestamp - currentBlockTimestamp,
+                        ),
+                    };
+                default:
+                    return {
+                        block: {
+                            number: pos,
+                            timestamp: currentBlockTimestamp,
+                            date: new Date(currentBlockTimestamp * 1000),
+                        },
+                        retries: depth,
+                        secondsFromTarget: Math.ceil(
+                            this.timestamp - currentBlockTimestamp,
+                        ),
+                    };
+            }
         }
 
         if (this.timestamp < currentBlockTimestamp) {
-            return this.checkBlock(pos, low, adjustedBlockTime, depth + 1);
+            return this.checkBlock(
+                pos,
+                low,
+                adjustedBlockTime,
+                position,
+                depth + 1,
+            );
         }
 
-        return this.checkBlock(high, pos, adjustedBlockTime, depth + 1);
+        return this.checkBlock(
+            high,
+            pos,
+            adjustedBlockTime,
+            position,
+            depth + 1,
+        );
     }
 
     private async getInitialTimestamp() {
-        const firstBlock = await this.nodeClient.getBlock(1);
-
-        this.initialTimestamp = Number(firstBlock.timestamp);
+        if (!this.initialTimestamp) {
+            const firstBlock = await this.nodeClient.getBlock(1);
+            this.initialTimestamp = Number(firstBlock.timestamp);
+        }
     }
 
     private getTimestamp(_date: number | string | Date) {
